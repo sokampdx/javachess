868fdf33886f1e03eef214634694bbfbe04a68a3
sokam-maclinux <sokam-maclinux@users.noreply.github.com>
2026-02-23 14:15:18 -0800

    add Queen

---
+++ app/src/main/java/Queen.java
@@ -0,0 +1,37 @@
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+public class Queen extends Piece {
+    @Override
+    public boolean isValidMove(Position newPosition, ChessBoard board) {
+        int rowDiff = Math.abs(newPosition.getRow() - position.getRow());
+        int colDiff = Math.abs(newPosition.getCol() - position.getCol());
+
+        if (rowDiff == colDiff || newPosition.getRow() == position.getRow() || newPosition.getCol() == position.getCol()) {
+            // Check if the path is clear
+            int rowDirection = Integer.signum(newPosition.getRow() - position.getRow());
+            int colDirection = Integer.signum(newPosition.getCol() - position.getCol());
+
+            int currentRow = position.getRow() + rowDirection;
+            int currentCol = position.getCol() + colDirection;
+
+            while (currentRow != newPosition.getRow() || currentCol != newPosition.getCol()) {
+                if (!board.isEmpty(currentRow, currentCol)) {
+                    return false; // Path is blocked
+                }
+                currentRow += rowDirection;
+                currentCol += colDirection;
+            }
+
+            return board.isEmpty(newPosition.getRow(), newPosition.getCol()) ||
+                   board.getPiece(newPosition.getRow(), newPosition.getCol()).getColor() != this.color;
+        }
+
+        return false;
+    }
+    
+}
