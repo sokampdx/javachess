6ee69399c88db2ddd3d917f55202162e37408fa2
sokam-maclinux <sokam-maclinux@users.noreply.github.com>
2026-02-23 13:42:12 -0800

    add en passant check

--- app/src/main/java/Pawn.java
+++ app/src/main/java/Pawn.java
@@ -1,7 +1,18 @@
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
+@Getter
+@Setter
 @SuperBuilder
 public class Pawn extends Piece {
+    private boolean canEnPassant;
+
+    public Pawn(PieceColor color, Position position) {
+        super(color, position);
+        this.canEnPassant = false;
+    }
+
     @Override
     public boolean isValidMove(Position newPosition, ChessBoard board) {
         // Implement pawn movement logic here
@@ -17,15 +28,24 @@ public class Pawn extends Piece {
             // Initial double move
             if ((position.getRow() == 1 && color == PieceColor.WHITE) || (position.getRow() == 6 && color == PieceColor.BLACK)) {
                 if (rowDiff == 2 * forwardDirection) {
+                    this.canEnPassant = true; // Set en passant flag
                     return board.isEmpty(position.getRow() + forwardDirection, position.getCol()) &&
                            board.isEmpty(newPosition.getRow(), newPosition.getCol());
                 }
             }
         } else if (Math.abs(colDiff) == 1 && rowDiff == forwardDirection) {
-            // Capture move
-            return board.isNotEmpty(newPosition.getRow(), newPosition.getCol()) &&
-                   board.getPiece(newPosition.getRow(), newPosition.getCol()).getColor() != this.color;
+            if (board.isNotEmpty(newPosition.getRow(), newPosition.getCol())) {
+                // Capture move
+                return board.getPiece(newPosition.getRow(), newPosition.getCol()).getColor() != this.color;
+            } else {
+                // En passant
+                return board.getPiece(newPosition.getRow(), newPosition.getCol()) == null && 
+                board.getPiece(newPosition.getRow() - forwardDirection, newPosition.getCol()).getColor() != this.color &&
+                board.getPiece(newPosition.getRow() - forwardDirection, newPosition.getCol()) instanceof Pawn &&
+                ((Pawn) board.getPiece(newPosition.getRow() - forwardDirection, newPosition.getCol())).canEnPassant;
+            }
         }
-        return true; // Placeholder
+
+        return false;
     }
 }
