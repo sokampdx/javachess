542b5204e23ee4e1cb23667710882c6fe056213f
sokam-maclinux <sokam-maclinux@users.noreply.github.com>
2026-02-26 12:27:32 -0800

    add test

--- app/src/test/java/org/github/sokampdx/ChessBoardTest.java
+++ app/src/test/java/org/github/sokampdx/ChessBoardTest.java
@@ -1,4 +1,43 @@
 package org.github.sokampdx;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class ChessBoardTest {
+    private ChessBoard board;
+
+    @Before
+    public void setUp() {
+        board = new ChessBoard();
+    }
+
+    @Test
+    public void initBoardIsEmpty() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8;j++) {
+                assertTrue(board.isEmpty(i, j));
+            }
+        }
+    }
+
+    @Test
+    public void setUpBoardCorrectly() {
+        board.setupNewBoard();
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8;j++) {
+                switch(i) {
+                    case 0:
+                    case 1:
+                    case 6:
+                    case 7:
+                        assertTrue(board.isNotEmpty(i, j));
+                        break;
+                    default:
+                        assertTrue(board.isEmpty(i, j));
+                }
+            }
+        }
+    }
+
 }
---
+++ app/src/test/java/org/github/sokampdx/RookTest.java
@@ -0,0 +1,43 @@
+package org.github.sokampdx;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RookTest {
+    private ChessBoard board;
+    private Rook whiteRook;
+
+    @Before
+    public void setUp() {
+        board = new ChessBoard();
+        whiteRook = new Rook(PieceColor.WHITE, new Position(0, 0), true);
+        board.setPiece(whiteRook.getPosition(), whiteRook);
+    }
+
+    @Test
+    public void testValidHorizontalMove() {
+        Position position = new Position(0, 5);
+        assertTrue(whiteRook.isValidMove(position, board));
+    }
+
+    @Test
+    public void testInvalidDiagonalMove() {
+        Position position = new Position(5, 5);
+        assertFalse(whiteRook.isValidMove(position, board));
+    }
+
+    @Test
+    public void testInvalidBlockedMove() {
+        Position position = new Position(0, 5);
+        Pawn blockingPawn = new Pawn(PieceColor.WHITE, new Position(0, 3), false);
+        board.setPiece(blockingPawn.getPosition(), blockingPawn);
+        assertFalse(whiteRook.isValidMove(position, board));
+    }
+
+    @Test
+    public void testValidVerticalMove() {
+        Position position = new Position(5, 0);
+        assertTrue(whiteRook.isValidMove(position, board));
+    }
+}
